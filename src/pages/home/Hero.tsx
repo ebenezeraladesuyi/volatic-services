1ec4b8e922104f8aea290395a80c951db54bceb6
Eben's <mac@MacBook-Air.local>
2026-04-30 14:43:23 +0100

    logo

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,8 +1,8 @@
-// import React from 'react'
+
 
 const Hero = () => {
   return (
-    <div className="font-extrabold w-full min-h-screen flex items-center justify-center">
+    <div className="font-extrabold w-full min-h-screen flex items-center justify-center bg-green-400">
       Hero
     </div>
   )
