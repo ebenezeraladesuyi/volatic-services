9f98bf218af641aac5f00eba642c1a5778a11827
Eben's <mac@MacBook-Air.local>
2026-04-30 15:03:14 +0100

    header-update

--- src/components/block/Header.tsx
+++ src/components/block/Header.tsx
@@ -1,9 +1,23 @@
 // import React from 'react'
 
+import { NavLink } from "react-router-dom"
+import logo from "../../assets/vol-logo.png"
+
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="px-10 py-2 flex justify-between">
+      <img src={logo} className="w-16" alt="" />
+
+      <div className="flex items-center gap-5">
+        <NavLink to="/about" className="p-2 bg-black text-white rounded">
+            <button>About</button>
+        </NavLink>
+
+
+        <NavLink to="/contact" className="p-2 bg-black text-white rounded ">
+            <button>Contact</button>
+        </NavLink>
+        </div>
     </div>
   )
 }
